55aeb63cf4d4d60f5719698bfd5e31c3e87183bb
marti <marti@LAPTOP-CLEMENT>
2022-11-08 11:14:08 +0100

    Ajoutez des fichiers projet.

---
+++ Spie-IOT-2/Controllers/DataController.cs
@@ -0,0 +1,49 @@
+﻿using InfluxDB.Client.Api.Domain;
+using InfluxDB.Client.Writes;
+using InfluxDB.Client;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using Spie_IOT_2;
+
+namespace Spie_IOT.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DataController : ControllerBase
+    {
+        public DataController()
+        {
+
+        }
+
+        [HttpPost(Name = "SendMeasure")]
+        public IActionResult SendMeasure([FromBody] SendMeasureBodyModel bodyModel)
+        {
+            try
+            {
+                var value = Convert.ToInt32(bodyModel.Data.Substring(2), 16);
+                var code = bodyModel.Data.Remove(2);
+
+                var influxDBClient = InfluxDBClientFactory.Create("http://localhost:8086", "FAWjcsemACi9nLiSo2Pw0edaZGkuwD8XYb2VAeRytUDVNtP1Q_0po1DghOqXbIffk0GDInhNLPlV2M-6XJdRAA==");
+
+                using (var writeApi = influxDBClient.GetWriteApi())
+                {
+                    var point = PointData.Measurement("temperature")
+                        .Tag("id", Guid.NewGuid().ToString())
+                        .Field("code", code)
+                        .Field("value", value);
+
+                    writeApi.WritePoint(point, "Test3", "Test");
+                }
+
+                influxDBClient.Dispose();
+
+                return Ok();
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
---
+++ Spie-IOT-2/SendMeasureBodyModel.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Spie_IOT_2
+{
+    public class SendMeasureBodyModel
+    {
+        public string Data { get; set; }
+    }
+}
